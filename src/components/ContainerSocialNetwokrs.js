89f946a047677f0a6f98a78b67fb13b5235c233a
spizzleayo <rajiayomikun@gmail.com>
2022-06-13 15:25:50 +0000

    added some components

--- src/components/ContainerSocialNetwokrs.js
+++ src/components/ContainerSocialNetwokrs.js
@@ -6,7 +6,7 @@ import { Link } from '@chakra-ui/layout';
 export default function ContainerSocialNetworks({e}) {
     return (
         <Fragment>
-            <Link href={e.url} pl={e.pl} textColor="white" pt={e.pt} isExternal display="flex" w="20px" h="20px" title={e.socialNetwork}>
+            <Link pl={e.pl} textColor="white" pt={e.pt} isExternal display="flex" w="20px" h="20px" title={e.socialNetwork}>
                 {parse(e.iconSvg)}
             </Link>
         </Fragment>
